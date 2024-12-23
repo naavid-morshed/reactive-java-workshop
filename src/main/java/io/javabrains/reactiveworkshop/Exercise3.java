4487740f9b74d181ed34b172564d8aae284be810
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-12-23 11:38:45 +0600

    Exercise 3 complete

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,17 +1,30 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class Exercise3 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
 
         // Use ReactiveSources.intNumbersFlux()
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
         // TODO: Write code here
+        final List<Integer> list = ReactiveSources.intNumbersFlux()
+                .toStream()
+                .toList();
 
+        list.forEach(System.out::println);
+        System.out.println(list.size());
+
+        // personal breakdown, inefficient
+//        final Flux<Integer> integerFlux = ReactiveSources.intNumbersFlux();
+//        final Mono<List<Integer>> listMono = integerFlux.collectList();
+//        final CompletableFuture<List<Integer>> future = listMono.toFuture();
+//        future.get().forEach(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
