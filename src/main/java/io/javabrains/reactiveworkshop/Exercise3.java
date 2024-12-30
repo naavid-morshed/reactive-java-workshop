b09627a90251fb1d9c177e090db4424aa2b8f6ee
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-12-30 15:08:42 +0600

    Exercise 3 complete

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -2,22 +2,19 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 public class Exercise3 {
 
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+    public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumbersFlux()
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
         // TODO: Write code here
-        final List<Integer> list = ReactiveSources.intNumbersFlux()
-                .toStream()
-                .toList();
+        final List<Integer> list = ReactiveSources.intNumbersFlux().toStream().toList();
 
-        list.forEach(System.out::println);
+        System.out.println("List is : " + list);
         System.out.println(list.size());
 
         // personal breakdown, inefficient
