0407ac82ab749b2fecd3abb6c575e0c502546be5
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2024-12-30 15:53:32 +0600

    Exercise 3 complete

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -17,14 +17,7 @@ public class Exercise3 {
         System.out.println("List is : " + list);
         System.out.println(list.size());
 
-        // personal breakdown, inefficient
-//        final Flux<Integer> integerFlux = ReactiveSources.intNumbersFlux();
-//        final Mono<List<Integer>> listMono = integerFlux.collectList();
-//        final CompletableFuture<List<Integer>> future = listMono.toFuture();
-//        future.get().forEach(System.out::println);
-
         System.out.println("Press a key to end");
         System.in.read();
     }
-
 }
