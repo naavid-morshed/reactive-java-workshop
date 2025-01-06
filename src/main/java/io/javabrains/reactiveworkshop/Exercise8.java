07154e913659ba0e4e0f92afd58d699ab15799b6
Naavid Morshed <naavid.morshed@g.bracu.ac.bd>
2025-01-06 18:14:27 +0600

    Exercise 8 completed

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -1,5 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
+import reactor.core.publisher.Flux;
+
 import java.io.IOException;
 
 public class Exercise8 {
@@ -10,24 +12,28 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-//        ReactiveSources.intNumbersFluxWithException()
-//                // doOnError does not override default error handling, rather extends. for overriding show error inside subscription
-////                .doOnError(err -> System.err.println("Error occurred: " + err.getMessage()))
-//                .subscribe(
-//                        integer -> {
-//                        },
-//                        // better for overriding default stack trace
-//                        err -> System.err.println(err.getMessage())
-//                ); // Default error logging still applies
+        ReactiveSources.intNumbersFluxWithException()
+                // doOnError does not override default error handling, rather extends. for overriding show error inside subscription
+//                .doOnError(err -> System.err.println("Error occurred: " + err.getMessage()))
+                .subscribe(
+                        integer -> {
+                        },
+                        // better for overriding default stack trace
+                        err -> System.err.println(err.getMessage())
+                ); // Default error logging still applies
 
         // Print values from intNumbersFluxWithException and continue on errors
-//        ReactiveSources.intNumbersFluxWithException()
-//                .onErrorContinue((throwable, o) -> System.out.println(throwable.getMessage()))
-//                .subscribe(System.out::println);
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorContinue((throwable, o) -> System.out.println(throwable.getMessage()))
+                .subscribe(System.out::println);
 
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorResume(throwable -> Flux.just(-1, -2))
+//                flux will ignore onErrorResume if onErrorContinue exists
+//                .onErrorContinue((throwable, o) -> {})
+                .subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
